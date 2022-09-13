7b2ac05bdb72d442ce8ce146a92213b25b856d3a
RiteshSingh9 <raunak.singh.visen.9@gmail.com>
2022-09-13 13:24:06 +0530

    form - schema validation with Yup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SimpleForm from './components/SimpleForm';
+import SimpleForm2 from './components/SimpleForm2';
 
 function App() {
   return (
     <div className="App">
-      <SimpleForm />
+      {/* <SimpleForm /> */}
+      <SimpleForm2 />
     </div>
   );
 }
---
+++ src/components/SimpleForm2.js
@@ -0,0 +1,128 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+import "./css/SimpleForm.css";
+
+
+function SimpleForm2() {
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [channel, setChannel] = useState('');
+    const initialValues = {
+        name: "Ritesh",
+        email: "",
+        channel: "",
+    };
+
+    const onSubmit = (values) => {
+        console.log("Form Data ", values);
+    };
+
+    const validate = (values) => {
+        /**
+         * Requirements to work properly
+         *  - this function must return and object names errors
+         *  - the keys of object must be same as fields name i.e. errors.channel errors.email errors.channel
+         *  - The values of these keys should be a string locating the message
+         * e.g. errors.name = 'This fields is required'
+         */
+
+        const email_pattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+        let errors = {};
+
+        if (!values.name) {
+            errors.name = "Name is Required !";
+        }
+
+        if (!values.email) {
+            errors.email = "Email is Required";
+        } else if (!email_pattern.test(values.email)) {
+            errors.email = "Invalid email format !";
+        }
+
+        if (!values.channel) {
+            errors.channel = "Channel name is Required !";
+        }
+
+        return errors;
+    };
+
+    // define validation Oject schema
+    const validationSchema = Yup.object({
+        name: Yup.string().required('Required'),
+        email: Yup.string().email('Invalid email Format').required('Required !'),
+        channel: Yup.string().required('Required !')
+    });
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        // validate,
+        validationSchema
+        // initialValues: initialValues,
+        // onSubmit: onSubmit,
+        // validate: validate
+        // validateOnBlur: validate
+    });
+
+    console.log('Form Visited fields ', formik.touched)
+
+    return (
+        // onBlue={formik.handleBlue} :- to keep track of visited fields in a form.
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="name"> Name </label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                />
+                {/* if form fields is visited and there is error then only show the error message */}
+                {formik.touched.name && formik.errors.name ? (
+                    <div className="errors">
+                        <strong>Error : </strong>
+                        {formik.errors.name}
+                    </div>
+                ) : null}
+
+                <label htmlFor="email">E-mail</label>
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                />
+                {formik.touched.email && formik.errors.email ? (
+                    <div className="errors">
+                        <strong>Error : </strong> {formik.errors.email}
+                    </div>
+                ) : null}
+
+                <label htmlFor="channel">Channel </label>
+                <input
+                    type="text"
+                    id="channel"
+                    name="channel"
+                    value={formik.values.channel}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                />
+                {formik.touched.channel && formik.errors.channel ? (
+                    <div className="errors">
+                        <strong>Error : </strong>
+                        {formik.errors.channel}
+                    </div>
+                ) : null}
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default SimpleForm2;
